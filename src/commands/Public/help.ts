946ce9ba1d880b831e1d56385883e3cf1116484b
Lawson Geter <71112863+lawsonDevvv@users.noreply.github.com>
2021-12-26 18:57:13 -0500

    feature(Command): revamp help command

--- src/commands/Public/help.ts
+++ src/commands/Public/help.ts
@@ -1,13 +1,28 @@
 import { ApplyOptions } from "@sapphire/decorators";
-import { Args, Command, CommandOptions } from "@sapphire/framework";
+import { Command, CommandOptions } from "@sapphire/framework";
 import { reply } from "@sapphire/plugin-editable-commands";
 import type { Message } from "discord.js";
 
 @ApplyOptions<CommandOptions>({
-    description: "Displays all commands and their descriptions"
+  description: "Displays all commands and their descriptions",
 })
 export default class extends Command {
-  async messageRun(message: Message, args: Args): Promise<Message | Message[]> {
-    return reply(message, `Soon:tm:.`);
+  async messageRun(message: Message): Promise<Message | Message[]> {
+    return reply(message, {
+      embeds: [
+        {
+          title: "Commands",
+          description: `${this.container.stores
+            .get("commands")
+            .map(
+              (command) =>
+                `\`a!${command.name} - ${command.category} - ${command.description}\``
+            )
+            .join("\n")}`,
+          footer: { text: "Axios v1.0.0 | Sapphire v2.2.1-stable" },
+          color: "BLUE",
+        },
+      ],
+    });
   }
 }
