10a374874145ab6682bd9a45a9dc66ed1c69e200
Lawson Geter <71112863+lawsonDevvv@users.noreply.github.com>
2021-12-26 19:24:14 -0500

    edit(Command): disable `a!rp`

--- src/commands/RP/rp.ts
+++ src/commands/RP/rp.ts
@@ -1,8 +1,12 @@
-import { Args, Command } from "@sapphire/framework";
+import { ApplyOptions } from "@sapphire/decorators";
+import { Args, Command, CommandOptions } from "@sapphire/framework";
 import { reply } from "@sapphire/plugin-editable-commands";
 import type { Message } from "discord.js";
 
-
+@ApplyOptions<CommandOptions>({
+    description: "Creates an RP session.",
+    enabled: false
+})
 export default class extends Command {
     async messageRun(message: Message, args: Args): Promise<Message | Message[]> {
         return reply(message, `Soon:tm:.`);
