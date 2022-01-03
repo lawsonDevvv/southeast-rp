976ae0608e6ec85e67150a39351b6ce430a1194b
Lawson Geter <71112863+lawsonDevvv@users.noreply.github.com>
2022-01-02 22:48:04 -0500

    sentry

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,6 @@
 import type { Message } from "discord.js";
 import { LawsonClient } from "./lib/LawsonClient";
+import * as Sentry from "@sentry/node";
 
 export const snipes = new Map<string, Message>();
 
@@ -10,6 +11,11 @@ async function main() {
     client.logger.info("Attempting to login...");
     await client.login(process.env.token ?? "0");
     client.logger.info("Successfully Logged In ✅");
+    
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+    })
   } catch (error) {
     client.logger.fatal("The token was empty. Please enter a valid token.");
     console.log(error);
