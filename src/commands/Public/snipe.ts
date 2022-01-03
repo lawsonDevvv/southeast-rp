06ae37afe1cb231543012f657ee0bc5044c3d895
Lawson Geter <71112863+lawsonDevvv@users.noreply.github.com>
2022-01-02 22:47:34 -0500

    idk what i did

--- src/commands/Public/snipe.ts
+++ src/commands/Public/snipe.ts
@@ -2,16 +2,26 @@ import { ApplyOptions } from "@sapphire/decorators";
 import { Command, CommandOptions } from "@sapphire/framework";
 import type { Message } from "discord.js";
 import { snipes } from "../../bot";
+import { captureException } from "@sentry/node";
 
 @ApplyOptions<CommandOptions>({
-    description: "Retrieve the last deleted message in the channel.",
-    typing: true,
+  description: "Retrieve the last deleted message in the channel.",
+  typing: true,
 })
 export default class extends Command {
-    async messageRun(message: Message) {
-        const snipe = snipes.get(message.channel.id);
-        console.log(snipes);
-        if (!snipes.has(message.channel.id)) return message.channel.send("There is no message to retrieve.");
-        return await message.channel.send(`The last deleted message in this from ${snipe?.author} in this channel was:\n\`\`\`${snipe?.content}\`\`\``);
+  async messageRun(message: Message) {
+    try {
+      const snipe = snipes.get(message.channel.id);
+      console.log(snipes);
+      if (!snipes.has(message.channel.id)) {
+        return message.channel.send("There is no message to retrieve.");
+      }
+      return await message.channel.send(
+        `The last deleted message in this from ${snipe?.author} in this channel was:\n\`\`\`${snipe?.content}\`\`\``
+      );
+    } catch (error) {
+      return captureException(error, { tags: { name: this.name } });
+    } finally {
     }
+  }
 }
