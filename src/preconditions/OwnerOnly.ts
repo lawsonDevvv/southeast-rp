0bc5b5d5f36de48267826e12c860e4150fcf307e
Lawson Geter <71112863+lawsonDevvv@users.noreply.github.com>
2022-01-06 16:27:53 -0500

    Made it yell at you if you try to run a restricted command

--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,11 +1,14 @@
 import { Precondition } from "@sapphire/framework";
 import type { Message } from "discord.js";
+import { disallowedResponses } from "src/bot";
 
 export class UserPrecondition extends Precondition {
   public async run(message: Message) {
+    const response = Math.floor(Math.random() * (10 - 0) + 0);
+
     return message.author.id === "415278805683404821"
       ? this.ok()
-      : message.channel.send("No.").then(() => this.error({message: "You are not Lawson."}));
+      : message.channel.send(disallowedResponses[response]).then(() => this.error({message: "You are not Lawson."}));
   }
 }
 
