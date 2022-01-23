68ac6f426818673bb2a54205b11782809259223f
lyingnet <schemes-breathy0t@icloud.com>
2022-01-22 18:20:09 -0800

    Added Database Functionality

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,6 @@
 import { LawsonClient } from "./lib/LawsonClient";
+import { container } from "@sapphire/framework";
+import { PrismaClient } from "@prisma/client";
 
 const client = new LawsonClient();
 
@@ -7,6 +9,7 @@ async function main() {
     client.logger.info("Attempting to login...");
     await client.login(process.env.token ?? "0");
     client.logger.info("Successfully Logged In ✅");
+    container.db = new PrismaClient();
   } catch (error) {
     client.logger.fatal("The token was empty. Please enter a valid token.");
     console.log(error);
