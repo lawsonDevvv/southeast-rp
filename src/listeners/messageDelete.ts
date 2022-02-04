0b19d3cfb30279da240e5160562573c033a75c44
lawsonDevvv <lawson@lawson.nyc>
2022-02-04 01:47:05 -0500

    logging for message deletions

--- src/listeners/messageDelete.ts
+++ src/listeners/messageDelete.ts
@@ -1,13 +1,33 @@
 import { ApplyOptions } from "@sapphire/decorators";
 import { Events, Listener, ListenerOptions } from "@sapphire/framework";
-import type { Message } from "discord.js";
+import { Message, MessageEmbed, TextChannel } from "discord.js";
 import { snipes } from "../bot";
 
 @ApplyOptions<ListenerOptions>({
-    event: Events.MessageDelete
+  event: Events.MessageDelete,
 })
 export default class extends Listener {
-    async run(message: Message) {
-        snipes.set(message.channel.id, message);
-    }
+  async run(message: Message) {
+    // snipe command stuff
+    // set channel id to message object in map
+
+    snipes.set(message.channel.id, message);
+
+    // admin logging
+
+    const channel = this.container.client.channels.cache.get(
+      "899190354421637160"
+    ) as TextChannel;
+
+    const embed = new MessageEmbed()
+      .setAuthor({
+        name: message.author.tag,
+        iconURL: message.author.displayAvatarURL({ dynamic: true }),
+      })
+      .setTitle("Message Deleted")
+      .setDescription(`${message.content}`)
+      .setTimestamp(Date.now());
+
+    channel.send({ embeds: [embed] });
+  }
 }
