43f7ac3db6441fe9f5716cc60f1e6f54c2f381c8
lawsonDevvv <lawson.geter@gmail.com>
2022-02-06 09:56:51 -0500

    feature: convert setStatus to slash commands

--- src/commands/Private/setStatus.ts
+++ src/commands/Private/setStatus.ts
@@ -1,15 +1,32 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { ApplyOptions } from "@sapphire/decorators";
-import { Args, Command, CommandOptions } from "@sapphire/framework";
-import type { Message } from "discord.js";
+import { ApplicationCommandRegistry, Command, CommandOptions } from "@sapphire/framework";
+import type { CommandInteraction } from "discord.js";
 
 @ApplyOptions<CommandOptions>({
-    description: "Set's the bot's status. Lawson only, bitch.",
-    preconditions: ['OwnerOnly'],
+  description: "Set's the bot's status. Lawson only, bitch.",
+  preconditions: ["OwnerOnly"],
 })
 export default class extends Command {
-    async messageRun(message: Message<boolean>, args: Args) {
-        const status = await args.rest("string");
-        await message.channel.send(`Setting status to \`${status}\``);
-        await message.client.user?.setActivity(status, { type: "PLAYING" });
-    }   
+  registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    const builder = new SlashCommandBuilder()
+      .setName("setstatus")
+      .setDescription("Set my status!")
+      .addStringOption((option) =>
+        option
+          .setName("status")
+          .setDescription("Status to change to.")
+          .setRequired(false)
+      );
+
+    registry.registerChatInputCommand(builder);
+  }
+
+  async chatInputRun(interaction: CommandInteraction) {
+    const status = interaction.options.getString("status");
+    await interaction.reply(`Setting status to \`${status}\``);
+    await this.container.client.user?.setActivity(status as string, {
+      type: "PLAYING",
+    });
+  }
 }
