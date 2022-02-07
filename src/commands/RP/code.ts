c580247e77a87ff6805bc5e597fed3acbf9c3968
lawsonDevvv <lawson.geter@gmail.com>
2022-02-07 18:51:47 -0500

    feature: code works with slash commands

--- src/commands/RP/code.ts
+++ src/commands/RP/code.ts
@@ -1,15 +1,23 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { ApplyOptions } from "@sapphire/decorators";
-import { Command, CommandOptions } from "@sapphire/framework";
-import { reply } from "@sapphire/plugin-editable-commands";
-import { Message, MessageEmbed, TextChannel } from "discord.js";
+import { ApplicationCommandRegistry, Command, CommandOptions } from "@sapphire/framework";
+import { CommandInteraction, MessageEmbed, TextChannel } from "discord.js";
 
 @ApplyOptions<CommandOptions>({
   description:
     "Generates a random number between 1000 and 9999 to be used as a code to an RP.",
 })
 export default class extends Command {
-  async messageRun(message: Message) {
-    const m = await reply(message, "Gimme a fuckin' second...");
+  async registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    const builder = new SlashCommandBuilder()
+      .setName(this.name)
+      .setDescription(this.description);
+    
+    registry.registerChatInputCommand(builder);
+  }
+
+  async chatInputRun(interaction: CommandInteraction) {
+    await interaction.reply("Gimme a fuckin' second...");
     const rng = Math.round(Math.random() * (9999 - 1000) + 1000);
 
     /** generate random number
@@ -18,18 +26,18 @@ export default class extends Command {
      */
 
     const embed = new MessageEmbed()
-      .setTitle(`RP Code Request by ${message.author.tag}`)
+      .setTitle(`RP Code Request by ${interaction.user.tag}`)
       .addField("Code Generated", rng.toString())
       .addField("\u2800", "\u2800", true)
       .addField("Date & Time Generated", `${new Date()} EST`, true)
       .setFooter({ text: "SoutheastOS v1.0.0 | Sapphire v2.2.1-stable" })
       .setTimestamp();
     (
-      message.guild?.channels.cache.get("925032510159736963") as TextChannel
+      interaction.guild?.channels.cache.get("925032510159736963") as TextChannel
     ).send({
       embeds: [embed],
     });
     // edit message with the number
-    m.edit(`${rng}`);
+    interaction.editReply(`${rng}`);
   }
 }
