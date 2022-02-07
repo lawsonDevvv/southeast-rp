f38561cc935a429c47f564f30febf6e76589a203
lawsonDevvv <lawson.geter@gmail.com>
2022-02-07 18:50:56 -0500

    feature: swagger command works w/ slash commands

--- src/commands/Admin/swagger.ts
+++ src/commands/Admin/swagger.ts
@@ -1,9 +1,13 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { ApplyOptions } from "@sapphire/decorators";
-import { Command, CommandOptions } from "@sapphire/framework";
-import { reply } from "@sapphire/plugin-editable-commands";
+import {
+  ApplicationCommandRegistry,
+  Command,
+  CommandOptions,
+} from "@sapphire/framework";
 import type {
   CategoryChannelResolvable,
-  Message,
+  CommandInteraction,
   TextChannel,
 } from "discord.js";
 
@@ -12,25 +16,34 @@ import type {
   preconditions: ["Nuke", "GuildTextOnly"],
 })
 export default class extends Command {
-  async messageRun(message: Message) {
-    // im lazy
-    const ch = message.channel as TextChannel;
+  registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    const builder = new SlashCommandBuilder()
+      .setName(this.name)
+      .setDescription(this.description);
 
-    await reply(
-      message,
-      `Missile target locked:\n|__ ${ch}\n\nIMMA CHARGHIN MAH LAZER`
+    registry.registerChatInputCommand(builder);
+  }
+
+  async chatInputRun(interaction: CommandInteraction) {
+    // im lazy
+    // CASTING BC YES
+    // DONT QUESTION MY MOTIVES
+    const ch = interaction.channel as TextChannel;
+
+    await interaction.reply(
+      `Missile target locked: ${interaction.channel}\n|__ ${ch}\n\nIMMA CHARGHIN MAH LAZER`
     );
 
     setTimeout(() => {
       ch.clone({
         parent: ch.parent as CategoryChannelResolvable,
         position: ch.rawPosition,
-        reason: `Hiroshima | This is ${message.author.tag}'s fault.`,
+        reason: `Hiroshima | This is ${interaction.user}'s fault.`,
       }).then((c) => {
         c.send(`I call this one the World War II Hydrogen Bomb.`);
       });
 
-      ch.delete(`Hiroshima | This is ${message.author.tag}'s fault.`);
+      ch.delete(`Hiroshima | This is ${interaction.user.tag}'s fault.`);
     }, 4000);
   }
 }
