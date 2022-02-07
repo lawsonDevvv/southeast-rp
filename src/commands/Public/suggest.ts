3a22531c065abc1d0da943d2b508f6553f96bd80
lawsonDevvv <lawson.geter@gmail.com>
2022-02-07 18:51:38 -0500

    feature: suggest works with slash commands

--- src/commands/Public/suggest.ts
+++ src/commands/Public/suggest.ts
@@ -1,35 +1,88 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { ApplyOptions } from "@sapphire/decorators";
-import { Args, Command, CommandOptions } from "@sapphire/framework";
-import { reply } from "@sapphire/plugin-editable-commands";
-import { Message, MessageEmbed, TextChannel } from "discord.js";
+import {
+  ApplicationCommandRegistry,
+  Command,
+  CommandOptions,
+} from "@sapphire/framework";
+import {
+  CommandInteraction,
+  MessageEmbed,
+  TextChannel,
+} from "discord.js";
 import minutes from "../../util/time/Minutes";
 
 @ApplyOptions<CommandOptions>({
   description: "Makes a suggestion to be voted on in the suggestion channel.",
-  cooldownDelay: minutes(1)
+  cooldownDelay: minutes(1),
 })
 export default class extends Command {
-  async messageRun(message: Message, args: Args): Promise<Message> {
-    if (args.finished) {
-      return message.channel.send(
-        "My dude, you wanna like, actually give me something to suggest?"
+  registerApplicationCommands(registry: ApplicationCommandRegistry) {
+    const builder = new SlashCommandBuilder()
+      .setName(this.name)
+      .setDescription(this.description)
+      .addStringOption((option) =>
+        option
+          .setName("suggestion")
+          .setDescription("What you want to suggest!")
+          .setRequired(true)
       );
-    }
 
-    const suggestion = await args.rest("string");
+    registry.registerChatInputCommand(builder);
+  }
 
-    // Suggestion Channel - Izzy's probably going to delete it/nuke it so this will be changed frequently.
-    const sugestionChannel = message.guild?.channels.cache.get(
+  // async messageRun(message: Message, args: Args): Promise<Message> {
+  //   if (args.finished) {
+  //     return message.channel.send(
+  //       "My dude, you wanna like, actually give me something to suggest?"
+  //     );
+  //   }
+
+  //   const suggestion = await args.rest("string");
+
+  //   // Suggestion Channel - Izzy's probably going to delete it/nuke it so this will be changed frequently.
+  //   const sugestionChannel = message.guild?.channels.cache.get(
+  //     "898836400240205824"
+  //   ) as TextChannel;
+
+  //   const suggestionEmbed = new MessageEmbed()
+  //     .setAuthor({
+  //       name: message.author.tag,
+  //       iconURL: message.author.displayAvatarURL({ dynamic: true }),
+  //     })
+  //     .setColor("GREY")
+  //     .setTitle(`Suggestion from ${message.author.tag}`)
+  //     .setDescription(`${suggestion}`)
+  //     .setTimestamp(Date.now());
+
+  //   const suggestionMessage = await sugestionChannel.send({
+  //     embeds: [suggestionEmbed],
+  //   });
+
+  //   suggestionMessage.react("<:Yes:899074935299911780>");
+  //   suggestionMessage.react("<:No:899074935014686720>");
+
+  //   const responseEmbed = new MessageEmbed().setDescription(
+  //     `Done! You can view your suggestion by [clicking me](https://discord.com/channels/${message.guild?.id}/${suggestionMessage.channel.id}/${suggestionMessage.id})!`
+  //   );
+
+  //   return await reply(message, { embeds: [responseEmbed] });
+  // }
+
+  async chatInputRun(interaction: CommandInteraction) {
+    const suggestion = interaction.options.getString("suggestion");
+
+    const sugestionChannel = interaction.guild?.channels.cache.get(
       "898836400240205824"
     ) as TextChannel;
 
     const suggestionEmbed = new MessageEmbed()
       .setAuthor({
-        name: message.author.tag,
-        iconURL: message.author.displayAvatarURL({ dynamic: true }),
+        name: interaction.user.tag,
+        iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
       })
       .setColor("GREY")
-      .setTitle(`Suggestion from ${message.author.tag}`)
+      .setTitle(`Suggestion from ${interaction.user.tag}`)
       .setDescription(`${suggestion}`)
       .setTimestamp(Date.now());
 
@@ -39,11 +92,11 @@ export default class extends Command {
 
     suggestionMessage.react("<:Yes:899074935299911780>");
     suggestionMessage.react("<:No:899074935014686720>");
-      
+
     const responseEmbed = new MessageEmbed().setDescription(
-      `Done! You can view your suggestion by [clicking me](https://discord.com/channels/${message.guild?.id}/${suggestionMessage.channel.id}/${suggestionMessage.id})!`
+      `Done! You can view your suggestion by [clicking me](https://discord.com/channels/${interaction.guild?.id}/${suggestionMessage.channel.id}/${suggestionMessage.id})!`
     );
 
-    return await reply(message, { embeds: [responseEmbed] });
+    interaction.reply({embeds: [responseEmbed]})
   }
 }
